bc74116b598a899869feadb51bf50cb69b03eb90
KelbyCodes <kelbycodes@gmail.com>
2023-09-20 16:21:58 -0500

    more work on API

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const req = require('express')();
+
+const app = express();
+
+app.get('/', (request, response) => {
+    response.json({
+        message: 'Automate all the things!',
+        timestamp: $now()
+    })
+});
+
+const port = process.env.PORT || 8080;
+
+// Testing by printing used port once app is running properly via node
+app.listen(port, () => console.log(`App is listening on http://localhost:${port}`));
